b5ab5500d156ce1cf54c64138cf28ec0f6cd9fe7
SK83RJOSH <sk83rjosh83@yahoo.com>
2015-09-04 23:22:28 -0400

    Resolves #1

--- server/c4spawner.lua
+++ server/c4spawner.lua
@@ -1,13 +1,34 @@
 class "C4Spawner"
 
+C4Spawner.MaxC4 = 10
+C4Spawner.SpawnDelay = 1
+
 function C4Spawner:__init()
 	self.timer = Timer()
+	self.playerData = {}
 
+	Events:Subscribe("ModuleLoad", self, self.ModuleLoad)
+	Events:Subscribe("PlayerJoin", self, self.PlayerJoin)
 	Events:Subscribe("PlayerQuit", self, self.PlayerQuit)
 	Events:Subscribe("EntityDespawn", self, self.EntityDespawn)
 	Events:Subscribe("ModuleUnload", self, self.ModuleUnload)
 	Network:Subscribe("Spawn", self, self.Spawn)
 	Network:Subscribe("Detonate", self, self.Detonate)
+end
+
+function C4Spawner:ModuleLoad()
+	for player in Server:GetPlayers() do
+		C4Spawner:PlayerJoin({player = player})
+	end
+end
+
+function C4Spawner:PlayerJoin(args)
+	local player = args.player
+
+	self.playerData[player:GetId()] = {
+		count = 0,
+		timer = Timer()
+	}
 end
 
 function C4Spawner:PlayerQuit(args)
@@ -18,6 +39,8 @@ function C4Spawner:PlayerQuit(args)
 			c4:Remove()
 		end
 	end
+
+	self.playerData[player:GetId()] = nil
 end
 
 function C4Spawner:EntityDespawn(args)
@@ -28,6 +51,7 @@ function C4Spawner:EntityDespawn(args)
 
 		if parent and parent.__type == entity.__type and parent == entity then
 			c4:Remove()
+			self.GetId[c4:GetOwner():GetId()] = self.GetId[c4:GetOwner():GetId()] - 1
 		end
 	end
 end
@@ -39,18 +63,37 @@ function C4Spawner:ModuleUnload()
 end
 
 function C4Spawner:Spawn(args, sender)
-	args.values.type = C4.__type
-	args.values.owner = sender
+	local playerData = self.playerData[sender:GetId()]
 
-	WorldNetworkObject.Create(args):SetStreamDistance(500)
+	if playerData.count < C4Spawner.MaxC4 then
+		local timeRemaining = math.max(math.floor(C4Spawner.SpawnDelay - playerData.timer:GetSeconds()), 0)
+
+		if timeRemaining == 0 then
+			playerData.timer:Restart()
+			playerData.count = playerData.count + 1
+			
+			args.values.type = C4.__type
+			args.values.owner = sender
+
+			WorldNetworkObject.Create(args):SetStreamDistance(500)
+		else
+			sender:SendChatMessage("Please wait " .. timeRemaining .. " second" .. (timeRemaining ~= 1 and "s" or "") .. " before placing another satchel!", Color.Yellow)
+		end
+	else
+		sender:SendChatMessage("You cannot place more than " .. C4Spawner.MaxC4 .. " satchel" .. (C4Spawner.MaxC4 ~= 1 and "s" or "") .. " at a time!", Color.Yellow)
+	end
 end
 
 function C4Spawner:Detonate(args, sender)
+	local playerData = self.playerData[sender:GetId()]
+
 	for k, c4 in pairs(C4Manager.C4) do
 		if c4:GetOwner() == sender then
 			c4:Detonate()
 		end
 	end
+
+	playerData.count = 0
 end
 
 C4Spawner = C4Spawner()
